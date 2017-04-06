3317b626d454bf668ae2dd3a0d33b07099e47058
Lawrence Law <d201.c317@gmail.com>
2017-04-06 02:30:33 +0100

    User Authentication

--- controllers/users/index.js
+++ controllers/users/index.js
@@ -1,11 +1,27 @@
 'use strict';
 
-const express = require('express');
-const router = express.Router();
+const Express   = require('express');
+const Router    = Express.Router();
+const Passport  = require('passport');
+const Helper    = require('../../helpers');
 
 /* GET users listing. */
-router.get('/', (req, res, next) => {
+Router.get('/', Helper.ensureAuthenticated, (req, res, next) => {
   res.send('respond with a resource');
 });
 
-module.exports = router;
+Router.get('/login', (req, res, next) => {
+  res.render('user/login', {
+    breadcrumb: true,
+    csrfToken: req.csrfToken(),
+    title: 'Login'
+  });
+});
+
+Router.post('/login', Passport.authenticate('local', {
+  successRedirect: '/users',
+  failureRedirect: '/users/login',
+  failureFlash: true
+}));
+
+module.exports = Router;
