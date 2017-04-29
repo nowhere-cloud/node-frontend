297af855bcbab763aa1baa604aba649c81545c92
Lawrence Law <d201.c317@gmail.com>
2017-04-29 18:35:02 +0100

    Moved stringify into controller

--- controllers/admin/instances.js
+++ controllers/admin/instances.js
@@ -17,7 +17,7 @@ Router.get('/', (req, res, next) => {
 Router.get('/partials/vm-list-hyp', (req, res, next) => {
   HTTP.GetJSON('http://api:3000/xen/vm/').then((data) => {
     res.render('_partials/vm-partials/vm-list', {
-      data: data
+      data: JSON.stringify(data, null, 2)
     });
   }).catch((e) => {
     return next(e);
@@ -27,7 +27,7 @@ Router.get('/partials/vm-list-hyp', (req, res, next) => {
 Router.get('/partials/vmtpl-list-hyp', (req, res, next) => {
   HTTP.GetJSON('http://api:3000/xen/vm/templates').then((data) => {
     res.render('_partials/vm-partials/tpl-list', {
-      data: data
+      data: JSON.stringify(data, null, 2)
     });
   }).catch((e) => {
     return next(e);
@@ -37,7 +37,7 @@ Router.get('/partials/vmtpl-list-hyp', (req, res, next) => {
 Router.get('/partials/net-list-hyp', (req, res, next) => {
   HTTP.GetJSON('http://api:3000/xen/net/').then((data) => {
     res.render('_partials/vm-partials/net-list', {
-      data: data
+      data: JSON.stringify(data, null, 2)
     });
   }).catch((e) => {
     return next(e);
@@ -47,7 +47,7 @@ Router.get('/partials/net-list-hyp', (req, res, next) => {
 Router.get('/partials/vif-list-hyp', (req, res, next) => {
   HTTP.GetJSON('http://api:3000/xen/vif/').then((data) => {
     res.render('_partials/vm-partials/vif-list', {
-      data: data
+      data: JSON.stringify(data, null, 2)
     });
   }).catch((e) => {
     return next(e);
