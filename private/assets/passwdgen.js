79d94e0e2033afea9eeb3349c90c291de15565dc
Lawrence Law <d201.c317@gmail.com>
2017-05-02 16:52:50 +0100

    Compiled version for passwd gen. extracted

---
+++ private/assets/passwdgen.js
@@ -0,0 +1,1 @@
+"use strict";var GeneratePasswordFunction=function(){var t=Math.random().toString(36).slice(-10),r=Math.random().toString(36).slice(-10);return String(t+r).replace(/\./g,"a").split("").sort(function(){return.5-Math.random()}).join("")};
